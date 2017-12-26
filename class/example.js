00def5d4551f56d34a2d89e8fb9d3422ebea77fd
Lewis <lewis.wei@foxmail.com>
2017-12-26 11:02:34 +0800

    class

--- class/example.js
+++ class/example.js
@@ -1,78 +1,72 @@
 class Point {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-    }
+  constructor(x, y) {
+    this.x = x
+    this.y = y
+  }
 
-    toString() {
-        return '(' + this.x + ', ' + this.y + ')';
-    }
+  toString() {
+    return '(' + this.x + ', ' + this.y + ')'
+  }
 }
 
 Object.assign(Point.prototype, {
-    toString2() {},
-    toValue() {}
+  toString2() {},
+  toValue() {}
 })
 
-var p = new Point(1, 2);
+var p = new Point(1, 2)
 
 console.log(Object.getOwnPropertyNames(Point.prototype))
 
 console.log(Object.keys(Point.prototype))
 
-
 class Foo {
-    constructor() {
-        return Object.create(null);
-    }
+  constructor() {
+    return Object.create(null)
+  }
 }
-
-
 
 console.log(new Foo() instanceof Foo)
 
 //定义类
 class Point2 {
+  constructor(x, y) {
+    this.x = x
+    this.y = y
+  }
 
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    toString() {
-        return '(' + this.x + ', ' + this.y + ')';
-    }
-
+  toString() {
+    return '(' + this.x + ', ' + this.y + ')'
+  }
 }
 
-var point = new Point2(2, 3);
+var point = new Point2(2, 3)
 
 point.toString() // (2, 3)
 
-console.log(point.hasOwnProperty('x'), // true
-    point.hasOwnProperty('y'), // true
-    point.hasOwnProperty('toString'), // false
-    point.__proto__.hasOwnProperty('toString')) // true)
+console.log(
+  point.hasOwnProperty('x'), // true
+  point.hasOwnProperty('y'), // true
+  point.hasOwnProperty('toString'), // false
+  point.__proto__.hasOwnProperty('toString')
+) // true)
 
 class Logger {
-    constructor() {
-        // this.printName = this.printName.bind(this);
+  constructor() {
+    // this.printName = this.printName.bind(this);
+  }
+  printName(name = 'there') {
+    var that = this
+    that.print(`Hello ${name}`)
+  }
 
-    }
-    printName(name = 'there') {
-        var that = this
-        that.print(`Hello ${name}`);
-    }
-
-    print(text) {
-        console.log(text);
-    }
+  print(text) {
+    console.log(text)
+  }
 }
 
-const logger = new Logger();
-const {
-    printName
-} = logger;
+const logger = new Logger()
+const { printName } = logger
 console.log(logger)
 // class MyClass {
 //   static myStaticProp = 42;
--- vue-bind/index.js
+++ vue-bind/index.js
@@ -30,3 +30,44 @@ var d = Object.assign({},b)
 for(let i in d){
     console.log(i)
 }
+
+// var f = Object.create(b,{a:1})
+// console.log(f)
+
+// Shape - superclass
+function Shape() {
+  this.x = -1;
+  this.y = 0;
+}
+
+// superclass method
+Shape.prototype.move = function(x, y) {
+  this.x += x;
+  this.y += y;
+  console.info('Shape moved.'+this.x+this.y);
+};
+
+// Rectangle - subclass
+function Rectangle() {
+  Shape.call(this); // call super constructor.
+}
+console.log(Shape.prototype)
+// subclass extends superclass
+
+// Rectangle = Object.create(Shape);
+Rectangle.prototype = Object.create(Shape.prototype);
+Rectangle.prototype.constructor = Rectangle;
+
+var rect = new Rectangle();
+
+console.log('Is rect an instance of Rectangle?',
+  rect instanceof Rectangle); // true
+console.log('Is rect an instance of Shape?',
+  rect instanceof Shape); // true
+rect.move(1, 1); // Outputs, 'Shape moved.'
+
+function aa(){
+
+}
+console.log(aa.constructor)
+var bb = new aa()
