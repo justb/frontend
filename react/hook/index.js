0e768413cbdeba2f96a1beb1ad017e16491e3c99
weiyuan <weiyuan@wecash.net>
2018-12-27 14:55:51 +0800

    index

--- react/hook/index.js
+++ react/hook/index.js
@@ -1,34 +1,30 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react'
 
 // 表单组件，有name, phone两个输入框。
-const App  = () => {
-  const name = useSetValue('hello');
-  const phone = useSetValue('120');
+const App = () => {
+  const name = useSetValue('hello')
+  const phone = useSetValue('120')
   useEffect(
     () => {
-      
       console.log('useEffect')
       return () => {
-        
-      };
+
+      }
     },
-    [a],
-  );
+    [a]
+  )
   return (
     <React.Fragment>
       <Item {...name} />
       <br />
       <Item {...phone} />
     </React.Fragment>
-  );
+  )
 }
 
-const a={}
-
-
-  
+const a = {}
 
 // controlled input component
 const Item = ({ value, setValue }) => (
@@ -37,29 +33,26 @@ const Item = ({ value, setValue }) => (
     <br />
     <input value={value} onChange={setValue} />
   </React.Fragment>
-);
+)
 
 // 可以将state连同handler function一起挪到组件外面。
 // 甚至可以export出去，让其他组件也能使用这个state逻辑
 const useSetValue = (initvalue) => {
-    console.log(initvalue)
-    console.log(useState(initvalue))
-  const [value, setValue] = useState(initvalue);
+  console.log(initvalue)
+  console.log(useState(initvalue))
+  const [value, setValue] = useState(initvalue)
   const handleChange = (e) => {
-    setValue(e.target.value);
+    setValue(e.target.value)
   }
   return {
     value,
-    setValue: handleChange,
-  };
+    setValue: handleChange
+  }
 }
 
-
-
-
-const root = document.getElementById('root')
+// const root = document.getElementById('root')
 
 ReactDOM.render(
-    <App></App>,
-    document.getElementById('root')
+  <App></App>,
+  document.getElementById('root')
 )
